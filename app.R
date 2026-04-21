6bc18ff0f3c973c1c969ca57c982453d85041465
vhgauto <victor.gauto@outlook.com>
2026-04-21 19:40:01 -0300

    panel serie temporal altura, agrego favicon, actualizo variables

--- app.R
+++ app.R
@@ -2,14 +2,20 @@ source("scripts/soporte.R")
 source("scripts/panel_mapa.R")
 source("scripts/panel_figura.R")
 source("scripts/panel_tabla.R")
+source("scripts/panel_serie_temporal.R")
 source("scripts/panel_publicaciones.R")
 source("scripts/panel_integrantes.R")
 
+# TODO: modelo secchi
+# TODO: serie temporal caudal
+# TODO: botones de descarga para tif
+
 ui <- page_navbar(
+  tags$head(tags$link(rel = "shortcut icon", href = "favicon.png")),
   title = a(
     HTML("Proyecto Paraná"),
     href = "https://019d775d-3cc3-1ed8-fc69-3a457d8f7a43.share.connect.posit.cloud/",
-    style = "text-decoration: none; color: #007e2e;"
+    style = glue("text-decoration: none; color: {verde};")
   ),
   navbar_options = navbar_options(
     bg = "#e5e5e5",
@@ -17,15 +23,17 @@ ui <- page_navbar(
   ),
   panel_mapa,
   panel_figura,
+  panel_tabla,
+  panel_serie_temporal,
   panel_publicaciones,
   panel_integrantes,
-  panel_tabla,
   nav_spacer(),
   nav_item(rrss_instagram),
   nav_item(rrss_github),
   footer = pie,
   theme = bs_theme(brand = "_brand.yml") |>
-    bs_add_rules(sass::sass_file("extras/mis_estilos.scss"))
+    bs_add_rules(sass::sass_file("extras/mis_estilos.scss")),
+  ff
 )
 
 server <- function(input, output) {
@@ -33,17 +41,17 @@ server <- function(input, output) {
   observeEvent(input$tipo, {
     tipo <- reactive(input$tipo)
     if (tipo() == "RGB") {
-      output$mymap <- renderLeaflet({
+      output$mapa_interactivo <- renderLeaflet({
         leaflet_rgb(input$fecha)
       })
     }
     if (tipo() == "Turbidez") {
-      output$mymap <- renderLeaflet({
+      output$mapa_interactivo <- renderLeaflet({
         leaflet_tipo(FECHA = input$fecha, TIPO = "turb")
       })
     }
     if (tipo() == "Profundidad de disco") {
-      output$mymap <- renderLeaflet({
+      output$mapa_interactivo <- renderLeaflet({
         leaflet_tipo(FECHA = input$fecha, TIPO = "secchi")
       })
     }
@@ -83,6 +91,106 @@ server <- function(input, output) {
       layout_columns(!!!l)
     })
   })
+
+  # SERIE TEMPORAL
+  condición_fechas <- reactive({
+    input$fecha_altura_min > input$fecha_altura_max
+  })
+  observeEvent(
+    condición_fechas(),
+    {
+      if (condición_fechas()) {
+        showModal(
+          modalDialog(
+            title = "Rango de fechas incorrecto.",
+            easyClose = TRUE,
+            "La fecha inicial debe ser menor a la fecha final.",
+            footer = modalButton("Cerrar")
+          )
+        )
+        output$serie_temporal_altura <- renderGirafe({
+          f_serie_temporal_altura(
+            FECHA_MIN = fecha_altura_min,
+            FECHA_MAX = fecha_altura_max
+          )
+        })
+      }
+    }
+  )
+
+  observeEvent(input$serie_temporal_ma, {
+    serie_temporal_ma <- reactive(input$serie_temporal_ma)
+    if (serie_temporal_ma()) {
+      output$slider_ma <- renderUI({
+        sliderInput(
+          "ma",
+          "Elegir tamaño de ventana, en días.",
+          min = 10,
+          max = 50,
+          value = 20,
+          step = seq(10, 50, 10)
+        )
+      })
+    } else {
+      output$slider_ma <- renderUI({
+        NULL
+      })
+    }
+  })
+
+  observeEvent(
+    list(
+      input$fecha_altura_min,
+      input$fecha_altura_max,
+      input$serie_temporal_ma,
+      input$ma
+    ),
+    {
+      output$serie_temporal_altura <- renderGirafe({
+        if (input$serie_temporal_ma && !is.null(input$ma)) {
+          f_serie_temporal_altura(
+            FECHA_MIN = input$fecha_altura_min,
+            FECHA_MAX = input$fecha_altura_max,
+            ma = TRUE,
+            n_ma = as.numeric(input$ma)
+          )
+        } else {
+          f_serie_temporal_altura(
+            FECHA_MIN = input$fecha_altura_min,
+            FECHA_MAX = input$fecha_altura_max,
+            ma = FALSE,
+            n_ma = 20
+          )
+        }
+      })
+    }
+  )
+
+  observeEvent(
+    list(
+      input$fecha_altura_min,
+      input$fecha_altura_max
+    ),
+    {
+      output$descarga_serie_temporal <- downloadHandler(
+        filename = function() {
+          paste0(
+            input$fecha_altura_min,
+            "_",
+            input$fecha_altura_max,
+            "_altura.csv"
+          )
+        },
+        content = function(file) {
+          f_descarga_serie_temporal(
+            FECHA_MIN = input$fecha_altura_min,
+            FECHA_MAX = input$fecha_altura_max,
+            FILE = file
+          )
+        }
+      )
+    }
+  )
 }
 
 shinyApp(ui, server)
