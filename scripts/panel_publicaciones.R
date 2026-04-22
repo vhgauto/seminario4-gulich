36c24bc3d660faeed6f9344025026e28f81f4e57
vhgauto <victor.gauto@outlook.com>
2026-04-22 20:08:09 -0300

    actualizo referencias

--- scripts/panel_publicaciones.R
+++ scripts/panel_publicaciones.R
@@ -1,26 +1,52 @@
 panel_publicaciones <- nav_panel(
   title = h5("Publicaciones"),
   card(
-    h4(
+    h3(
       icon_paper,
-      "Remote Sensing Regression Models to Estimate Water Quality Indicators in Continental Waters in North-East Argentina"
+      "Turbidity Estimation in Paraná River Middle Basin Using Remote Sensing Techniques"
     ),
     a(
       icon_doi,
-      "10.1109/ARGENCON62399.2024.10735875",
-      href = "https://doi.org/10.1109/ARGENCON62399.2024.10735875",
+      "10.1109/lagirs68367.2025.11414786",
+      href = "https://doi.org/10.1109/lagirs68367.2025.11414786",
       target = "_blank"
     ),
+    span(
+      icon_autor,
+      "Gauto V, Utgés E, Tenev D, Ferral A, Cardozo O, Acosta M, Gómez V, Geneyer V, Bonansea M",
+    ),
+    span(icon_año, "2025"),
     hr(),
-    h4(
+    h3(
       icon_paper,
       "Turbidity Estimation by Machine Learning Modelling and Remote Sensing Techniques Applied to a Water Treatment Plant"
     ),
     a(
       icon_doi,
       "10.13044/j.sdewes.d13.0539",
-      href = "http://dx.doi.org/10.13044/j.sdewes.d13.0539",
+      href = "https://doi.org/10.13044/j.sdewes.d13.0539",
       target = "_blank"
-    )
+    ),
+    span(
+      icon_autor,
+      "Gauto V, Utges E, Hervot E, Tenev MD, Farías A",
+    ),
+    span(icon_año, "2025"),
+    hr(),
+    h3(
+      icon_paper,
+      "Remote Sensing Regression Models to Estimate Water Quality Indicators in Continental Waters in North-East Argentina"
+    ),
+    a(
+      icon_doi,
+      "10.1109/argencon62399.2024.10735875",
+      href = "https://doi.org/10.1109/argencon62399.2024.10735875",
+      target = "_blank"
+    ),
+    span(
+      icon_autor,
+      "Gauto VH, Utgés EM, Hervot EI, Tenev MD, Farías A, Bonansea M, Ferral A",
+    ),
+    span(icon_año, "2024")
   )
 )
