d3a113b9b5b67e7321492b45b1f34f153c9af736
vhgauto <victor.gauto@outlook.com>
2026-04-26 00:39:26 -0300

    panel caudal para mostrar values boxes al hacer click en figura

---
+++ scripts/panel_caudal.R
@@ -0,0 +1,26 @@
+panel_caudal <- nav_panel(
+  title = h5(
+    span(
+      HTML('<span class="ph--flow-arrow-fill"></span>'),
+      "Caudal"
+    )
+  ),
+  layout_columns(
+    col_widths = c(9, 3),
+    card(
+      card_header(
+        span("Caudal del Río Paraná", class = "card-titulo")
+      ),
+      plotOutput("serie_caudal", click = clickOpts(id = "plot_click"))
+    ),
+    card(
+      card_header(
+        span("Contexto", class = "card-titulo")
+      ),
+      uiOutput("value_box_caudal")
+    )
+  )
+)
+
+# panel_caudal
+# 
