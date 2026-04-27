fb7a80204cf1dda3582c7be89bdfecc768aaa4f9
vhgauto <victor.gauto@outlook.com>
2026-04-27 14:38:07 -0300

    agrego títulos a cards, agrego ejemplo de reporte, actualizo botón de generar reporte

--- scripts/panel_quarto.R
+++ scripts/panel_quarto.R
@@ -13,31 +13,43 @@ panel_quarto <- nav_panel(
         card_header(span("Firma espectral", class = "card-titulo")),
         selectInput(
           "quarto_fecha_firma",
-          label = "Seleccionar fecha para firma espectral",
+          label = HTML("Seleccionar fecha para <b>firma espectral</b>"),
           choices = quarto_fecha_firma
-        )
+        ),
+        "Se creará una figura de la firma espectral para la fecha seleccionada, destacando las orillas de la Provincia de Chaco y Corrientes."
       ),
       card(
         card_header(span("Mapa", class = "card-titulo")),
         selectInput(
           "quarto_fecha_mapa",
-          label = "Seleccionar fecha para mapa de turbidez y profundidad de disco de Secchi.",
+          label = HTML(
+            "Seleccionar fecha para mapa de <b>turbidez</b> y <b>profundidad de disco de Secchi</b>"
+          ),
           choices = quarto_fecha_mapa
-        )
+        ),
+        "Se mostrarán dos mapas con la distribución espacial de los parámetros sobre la región de estudio en el Río Paraná."
       )
     ),
     card(
-      card_header(span("Ejemplo", class = "card-titulo")),
-      "Single card taking up the entire second column."
+      card_header(span("Ejemplo de reporte", class = "card-titulo")),
+      img(src = "p.png")
     ),
     layout_column_wrap(
       width = 1,
       fill = FALSE,
       input_task_button(
         id = "render_quarto",
-        label = span("Generar reporte", style = "font-size: 2em"),
+        label = span(
+          bsicons::bs_icon("pen-fill"),
+          "Generar reporte",
+          style = "font-size: 2em"
+        ),
         label_busy = span(
-          "El reporte estará listo en pocos segundos", style = "font-size: 2em"
+          "El reporte estará listo en pocos segundos",
+          style = "font-size: 2em"
+        ),
+        icon_busy = HTML(
+          '<span class="svg-spinners--6-dots-scale-middle"></span>'
         ),
         style = "height: 200px"
       ),
@@ -45,4 +57,3 @@ panel_quarto <- nav_panel(
     )
   )
 )
-# panel_quarto
